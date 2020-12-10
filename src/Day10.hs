9d24fa4fdb69ad6e42fe9a0a435c9a55acba975e
Julian Jelfs <julian.jelfs@ef.com>
2020-12-10 10:42:26 +0000

    day10

--- src/Day10.hs
+++ src/Day10.hs
@@ -6,6 +6,9 @@ import Data.List (foldl', sort)
 partOne :: IO Int
 partOne = countDiffs . sort <$> mapLines read "data/day10.txt"
 
+partTwo :: IO Int
+partTwo = countGroups . group . sort <$> mapLines read "data/day10.txt"
+
 countDiffs :: [Int] -> Int
 countDiffs =
   (\(ones, threes, _) -> ones * (threes + 1))
@@ -18,5 +21,27 @@ countDiffs =
       )
       (0, 0, 0)
 
-partTwo :: IO [(Int, [Int])]
-partTwo = undefined
+countGroups :: [[Int]] -> Int
+countGroups =
+  foldl'
+    ( \total group ->
+        case length group of
+          3 -> total * 2
+          4 -> total * 4
+          5 -> total * 7
+          _ -> total
+    )
+    1
+
+group :: [Int] -> [[Int]]
+group adapters = c : g
+  where
+    (g, c, _) =
+      foldl'
+        ( \(groups, currentGroup, prev) a ->
+            if a - prev == 1
+              then (groups, a : currentGroup, a)
+              else (currentGroup : groups, [a], a)
+        )
+        ([], [0], 0)
+        adapters
