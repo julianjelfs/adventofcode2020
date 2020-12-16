aff82de9762ac3598c1935d586599a1078b1eab9
Julian Jelfs <julian.jelfs@ef.com>
2020-12-16 11:55:44 +0000

    day16 part one

--- src/Day16.hs
+++ src/Day16.hs
@@ -3,23 +3,40 @@ module Day16 where
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import Data.List.Split (splitOn)
+import Debug.Trace (traceShowId)
 
 type FieldValues = M.Map String (S.Set Int)
 type Ticket = S.Set Int
 data Data = Data FieldValues Ticket [Ticket] deriving Show
 
-partOne :: IO Data 
-partOne = parseData
+numFields :: Int 
+numFields = 20
+
+partOne :: IO Int 
+partOne = do 
+    (Data fv _ nearby) <- parseData
+    pure $ sumOfInvalidNearbyValues (allValidValues fv) (traceShowId nearby)
+    where 
+        allValidValues :: FieldValues -> S.Set Int
+        allValidValues = 
+            M.foldr (\s all -> S.union s all) S.empty
+
+        sumOfInvalidNearbyValues :: S.Set Int -> [Ticket] -> Int
+        sumOfInvalidNearbyValues valid = 
+            foldr 
+                (\ticket sumInvalid ->
+                    sumInvalid + (sum $ S.difference ticket valid)
+                ) 0
 
 parseData :: IO Data
 parseData = do
     allLines <- lines <$> readFile "data/day16.txt"
     pure $ Data (fieldVals allLines) (myTicket allLines) (nearby allLines)
     where 
-        fieldValsStr = take 20 
+        fieldValsStr = take numFields 
         fieldVals = parseFieldVals . fieldValsStr
-        myTicket = parseTicket . head . take 1 . drop 22 
-        nearby = fmap parseTicket . drop 25 
+        myTicket = parseTicket . head . take 1 . drop (numFields + 2) 
+        nearby = fmap parseTicket . drop (numFields + 5) 
 
 parseTicket :: String -> Ticket 
 parseTicket str = S.fromList $ read <$> splitOn "," str
